097c2053d9b9546bf3bef92f1b7aba0c72b8214c
Des Herriott <des.herriott@gmail.com>
2019-11-29 07:55:12 +0000

    Fluid matching now supports fluid tags Also fix potential NPE (null fluidstack)

--- src/main/java/me/desht/modularrouters/logic/filter/matchers/FluidMatcher.java
+++ src/main/java/me/desht/modularrouters/logic/filter/matchers/FluidMatcher.java
@@ -1,7 +1,9 @@
 package me.desht.modularrouters.logic.filter.matchers;
 
+import com.google.common.collect.Sets;
 import me.desht.modularrouters.logic.filter.Filter;
 import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
@@ -10,17 +12,17 @@ public class FluidMatcher implements IItemMatcher {
     private final Fluid fluid;
 
     public FluidMatcher(ItemStack stack) {
-        fluid = FluidUtil.getFluidContained(stack).map(FluidStack::getFluid).orElse(null);
+        fluid = FluidUtil.getFluidContained(stack).map(FluidStack::getFluid).orElse(Fluids.EMPTY);
     }
 
     @Override
     public boolean matchItem(ItemStack stack, Filter.Flags flags) {
         return FluidUtil.getFluidContained(stack)
-                .map(fluidStack -> fluidStack.getFluid() == fluid)
+                .map(fluidStack -> matchFluid(fluidStack.getFluid(), flags))
                 .orElse(false);
     }
 
-    public boolean matchFluid(Fluid fluid) {
-        return fluid == this.fluid;
+    public boolean matchFluid(Fluid fluid, Filter.Flags flags) {
+        return fluid == this.fluid || flags.matchTags() && !Sets.intersection(fluid.getTags(), this.fluid.getTags()).isEmpty();
     }
 }
