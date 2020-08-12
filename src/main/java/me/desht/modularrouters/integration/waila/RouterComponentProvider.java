09fcb34db7e73c9e767b1853d9151b517a0ea9b0
Des Herriott <des.herriott@gmail.com>
2020-08-12 09:18:44 +0100

    Added back Hwyla

--- src/main/java/me/desht/modularrouters/integration/waila/RouterComponentProvider.java
+++ src/main/java/me/desht/modularrouters/integration/waila/RouterComponentProvider.java
@@ -1,27 +1,41 @@
 package me.desht.modularrouters.integration.waila;
 
-public class RouterComponentProvider /*implements IComponentProvider*/ {
-//    @Override
-//    public void appendBody(List<ITextComponent> tooltip, IDataAccessor accessor, IPluginConfig config) {
-//        CompoundNBT data = accessor.getServerData();
-//        if (accessor.getTileEntity() instanceof TileEntityItemRouter) {
-//            if (data.getBoolean("Denied")) {
-//                tooltip.add(MiscUtil.xlate("chatText.security.accessDenied"));
-//            } else {
-//                MiscUtil.appendMultilineText(tooltip, TextFormatting.WHITE,"itemText.misc.moduleCount", data.getInt("ModuleCount"));
-//                CompoundNBT upgrades = data.getCompound("Upgrades");
-//                for (String k : upgrades.keySet()) {
-//                    tooltip.add(MiscUtil.xlate("itemText.misc.upgradeCount", I18n.format(k), upgrades.getInt(k)));
-//                }
-//                RouterRedstoneBehaviour rrb = RouterRedstoneBehaviour.values()[data.getInt("RedstoneMode")];
-//                tooltip.add(MiscUtil.xlate("guiText.tooltip.redstone.label")
-//                        .appendText(": " + TextFormatting.AQUA)
-//                        .appendSibling(MiscUtil.xlate("guiText.tooltip.redstone." + rrb))
-//                );
-//                if (data.getBoolean("EcoMode")) {
-//                    tooltip.add(MiscUtil.xlate("itemText.misc.ecoMode"));
-//                }
-//            }
-//        }
-//    }
+import mcp.mobius.waila.api.IComponentProvider;
+import mcp.mobius.waila.api.IDataAccessor;
+import mcp.mobius.waila.api.IPluginConfig;
+import me.desht.modularrouters.block.tile.TileEntityItemRouter;
+import me.desht.modularrouters.logic.RouterRedstoneBehaviour;
+import me.desht.modularrouters.util.MiscUtil;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+
+import java.util.List;
+
+public class RouterComponentProvider implements IComponentProvider {
+    @Override
+    public void appendBody(List<ITextComponent> tooltip, IDataAccessor accessor, IPluginConfig config) {
+        CompoundNBT data = accessor.getServerData();
+        if (accessor.getTileEntity() instanceof TileEntityItemRouter) {
+            if (data.getBoolean("Denied")) {
+                tooltip.add(new TranslationTextComponent("chatText.security.accessDenied"));
+            } else {
+                MiscUtil.appendMultilineText(tooltip, TextFormatting.WHITE,"itemText.misc.moduleCount", data.getInt("ModuleCount"));
+                CompoundNBT upgrades = data.getCompound("Upgrades");
+                for (String k : upgrades.keySet()) {
+                    tooltip.add(new TranslationTextComponent("itemText.misc.upgradeCount", I18n.format(k), upgrades.getInt(k)));
+                }
+                RouterRedstoneBehaviour rrb = RouterRedstoneBehaviour.values()[data.getInt("RedstoneMode")];
+                tooltip.add(new TranslationTextComponent("guiText.tooltip.redstone.label")
+                        .appendString(": " + TextFormatting.AQUA)
+                        .append(new TranslationTextComponent("guiText.tooltip.redstone." + rrb))
+                );
+                if (data.getBoolean("EcoMode")) {
+                    tooltip.add(new TranslationTextComponent("itemText.misc.ecoMode"));
+                }
+            }
+        }
+    }
 }
--- src/main/java/me/desht/modularrouters/integration/waila/WailaIntegration.java
+++ src/main/java/me/desht/modularrouters/integration/waila/WailaIntegration.java
@@ -1,10 +1,16 @@
 package me.desht.modularrouters.integration.waila;
 
-//@WailaPlugin
-public class WailaIntegration /*implements IWailaPlugin*/ {
-//    @Override
-//    public void register(IRegistrar iRegistrar) {
-//        iRegistrar.registerBlockDataProvider(new RouterDataProvider(), BlockItemRouter.class);
-//        iRegistrar.registerComponentProvider(new RouterComponentProvider(), TooltipPosition.BODY, BlockItemRouter.class);
-//    }
+import mcp.mobius.waila.api.IRegistrar;
+import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.TooltipPosition;
+import mcp.mobius.waila.api.WailaPlugin;
+import me.desht.modularrouters.block.BlockItemRouter;
+
+@WailaPlugin
+public class WailaIntegration implements IWailaPlugin {
+    @Override
+    public void register(IRegistrar iRegistrar) {
+        iRegistrar.registerBlockDataProvider(new RouterDataProvider(), BlockItemRouter.class);
+        iRegistrar.registerComponentProvider(new RouterComponentProvider(), TooltipPosition.BODY, BlockItemRouter.class);
+    }
 }
