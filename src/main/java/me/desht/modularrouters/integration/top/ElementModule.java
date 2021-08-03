758352bd92471a35e535b52e2077ed5c5851af08
Des Herriott <des.herriott@gmail.com>
2021-08-03 13:58:56 +0100

    Added back TOP support Getting TOP via cursemaven for now Updated to forge 37.0.22

--- src/main/java/me/desht/modularrouters/integration/top/ElementModule.java
+++ src/main/java/me/desht/modularrouters/integration/top/ElementModule.java
@@ -1,45 +1,69 @@
 package me.desht.modularrouters.integration.top;
 
-public class ElementModule /*implements IElement*/ {
-//    private final ItemStack stack;
-//    private final ItemModule.RelativeDirection dir;
-//
-//    public ElementModule(ItemStack stack) {
-//        Validate.isTrue(stack.getItem() instanceof ItemModule, "provided item stack is not an ItemModule!");
-//        this.stack = stack;
-//        this.dir = ModuleHelper.getRelativeDirection(stack);
-//    }
-//
-//    public ElementModule(ByteBuf buf) {
-//        FriendlyByteBuf pb = new FriendlyByteBuf(buf);
-//        this.stack = pb.readItem();
-//        this.dir = ItemModule.RelativeDirection.values()[pb.readByte()];
-//    }
-//
-//    @Override
-//    public void render(PoseStack matrixStack, int x, int y) {
-//        GuiUtil.renderItemStack(matrixStack, Minecraft.getInstance(), stack, x + (getWidth() - 18) / 2, y + (getHeight() - 18) / 2, dir.getSymbol());
-//    }
-//
-//    @Override
-//    public int getWidth() {
-//        return 20;
-//    }
-//
-//    @Override
-//    public int getHeight() {
-//        return 20;
-//    }
-//
-//    @Override
-//    public void toBytes(FriendlyByteBuf buffer) {
-//        buffer.writeItem(stack);
-//        buffer.writeByte(dir.ordinal());
-//    }
-//
-//    @Override
-//    public int getID() {
-//        return TOPCompatibility.ELEMENT_MODULE_ITEM;
-//    }
+import com.mojang.blaze3d.vertex.PoseStack;
+import io.netty.buffer.ByteBuf;
+import mcjty.theoneprobe.api.IElement;
+import mcjty.theoneprobe.api.IElementFactory;
+import me.desht.modularrouters.client.util.GuiUtil;
+import me.desht.modularrouters.item.module.ModuleItem;
+import me.desht.modularrouters.util.ModuleHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.apache.commons.lang3.Validate;
 
+public class ElementModule implements IElement {
+    private final ItemStack stack;
+    private final ModuleItem.RelativeDirection dir;
+
+    public ElementModule(ItemStack stack) {
+        Validate.isTrue(stack.getItem() instanceof ModuleItem, "provided item stack is not an ItemModule!");
+        this.stack = stack;
+        this.dir = ModuleHelper.getRelativeDirection(stack);
+    }
+
+    public ElementModule(FriendlyByteBuf buf) {
+        FriendlyByteBuf pb = new FriendlyByteBuf(buf);
+        this.stack = pb.readItem();
+        this.dir = pb.readEnum(ModuleItem.RelativeDirection.class);
+    }
+
+    @Override
+    public void render(PoseStack matrixStack, int x, int y) {
+        GuiUtil.renderItemStack(matrixStack, Minecraft.getInstance(), stack, x + (getWidth() - 18) / 2, y + (getHeight() - 18) / 2, dir.getSymbol());
+    }
+
+    @Override
+    public int getWidth() {
+        return 20;
+    }
+
+    @Override
+    public int getHeight() {
+        return 20;
+    }
+
+    @Override
+    public void toBytes(FriendlyByteBuf buffer) {
+        buffer.writeItem(stack);
+        buffer.writeEnum(dir);
+    }
+
+    @Override
+    public ResourceLocation getID() {
+        return TOPCompatibility.ELEMENT_MODULE_ITEM;
+    }
+
+    public static class Factory implements IElementFactory {
+        @Override
+        public IElement createElement(FriendlyByteBuf buf) {
+            return new ElementModule(buf);
+        }
+
+        @Override
+        public ResourceLocation getId() {
+            return TOPCompatibility.ELEMENT_MODULE_ITEM;
+        }
+    }
 }
--- src/main/java/me/desht/modularrouters/integration/top/TOPInfoProvider.java
+++ src/main/java/me/desht/modularrouters/integration/top/TOPInfoProvider.java
@@ -1,34 +1,47 @@
 package me.desht.modularrouters.integration.top;
 
+import mcjty.theoneprobe.api.IProbeHitData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.ProbeMode;
+import me.desht.modularrouters.core.ModBlockEntities;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.items.IItemHandler;
+
 class TOPInfoProvider {
-//    static void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, Player player, Level world, BlockState blockState, IProbeHitData data) {
-//        ModularRouterBlockEntity.getRouterAt(world, data.getPos()).ifPresent(router -> {
-//            if (router.isPermitted(player)) {
-//                IItemHandler modules = router.getModules();
-//                IProbeInfo sub = probeInfo.horizontal();
-//                for (int i = 0; i < modules.getSlots(); i++) {
-//                    ItemStack stack = modules.getStackInSlot(i);
-//                    if (!stack.isEmpty()) {
-//                        sub.element(new ElementModule(stack));
-//                    }
-//                }
-//                sub = probeInfo.horizontal();
-//                IItemHandler upgrades = router.getUpgrades();
-//                for (int i = 0; i < upgrades.getSlots(); i++) {
-//                    ItemStack stack = upgrades.getStackInSlot(i);
-//                    if (!stack.isEmpty()) {
-//                        sub.item(stack);
-//                    }
-//                }
-//
-//                probeInfo.text(new TextComponent(ChatFormatting.YELLOW.toString())
-//                        .append(new TranslatableComponent("modularrouters.guiText.tooltip.redstone.label"))
-//                        .append(ChatFormatting.WHITE + ": ")
-//                        .append(new TranslatableComponent(router.getRedstoneBehaviour().getTranslationKey()))
-//                );
-//            } else {
-//                probeInfo.text(new TranslatableComponent("modularrouters.chatText.security.accessDenied"));
-//            }
-//        });
-//    }
+    static void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, Player player, Level world, BlockState blockState, IProbeHitData data) {
+        world.getBlockEntity(data.getPos(), ModBlockEntities.MODULAR_ROUTER.get()).ifPresent(router -> {
+            if (router.isPermitted(player)) {
+                IItemHandler modules = router.getModules();
+                IProbeInfo sub = probeInfo.horizontal();
+                for (int i = 0; i < modules.getSlots(); i++) {
+                    ItemStack stack = modules.getStackInSlot(i);
+                    if (!stack.isEmpty()) {
+                        sub.element(new ElementModule(stack));
+                    }
+                }
+                sub = probeInfo.horizontal();
+                IItemHandler upgrades = router.getUpgrades();
+                for (int i = 0; i < upgrades.getSlots(); i++) {
+                    ItemStack stack = upgrades.getStackInSlot(i);
+                    if (!stack.isEmpty()) {
+                        sub.item(stack);
+                    }
+                }
+
+                probeInfo.text(new TextComponent(ChatFormatting.YELLOW.toString())
+                        .append(new TranslatableComponent("modularrouters.guiText.tooltip.redstone.label"))
+                        .append(ChatFormatting.WHITE + ": ")
+                        .append(new TranslatableComponent(router.getRedstoneBehaviour().getTranslationKey()))
+                );
+            } else {
+                probeInfo.text(new TranslatableComponent("modularrouters.chatText.security.accessDenied"));
+            }
+        });
+    }
 }
