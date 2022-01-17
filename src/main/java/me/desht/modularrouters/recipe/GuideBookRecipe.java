e0c1bfc30650c16296d786aa53e8e0e6548b0ef2
Des Herriott <des.herriott@gmail.com>
2022-01-17 14:35:47 +0000

    Forward port guidebook/patchouli patch from 1.16

--- src/main/java/me/desht/modularrouters/recipe/GuideBookRecipe.java
+++ src/main/java/me/desht/modularrouters/recipe/GuideBookRecipe.java
@@ -4,6 +4,7 @@ import me.desht.modularrouters.core.ModItems;
 import me.desht.modularrouters.core.ModRecipes;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -28,7 +29,11 @@ public class GuideBookRecipe extends ShapelessRecipe {
 
     private static ItemStack makeGuideBook() {
         Item bookItem = ForgeRegistries.ITEMS.getValue(BOOK_ID);
-        if (bookItem == null) return ItemStack.EMPTY;
+        if (bookItem == null) {
+            ItemStack stack = new ItemStack(Items.PAPER);
+            stack.setHoverName(new TextComponent("Install Patchouli for the guide book!"));
+            return stack;
+        }
         ItemStack book = new ItemStack(bookItem);
         CompoundTag tag = book.getOrCreateTag();
         tag.putString(NBT_KEY, NBT_VAL);
