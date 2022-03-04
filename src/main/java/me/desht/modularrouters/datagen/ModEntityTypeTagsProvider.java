17aa622c55c42c8c945652c88ea1393fc272504a
Des Herriott <des.herriott@gmail.com>
2022-03-04 16:39:46 +0000

    Add villagers & wandering traders to interact blacklist

--- src/main/java/me/desht/modularrouters/datagen/ModEntityTypeTagsProvider.java
+++ src/main/java/me/desht/modularrouters/datagen/ModEntityTypeTagsProvider.java
@@ -4,6 +4,7 @@ import me.desht.modularrouters.ModularRouters;
 import me.desht.modularrouters.ModularRoutersTags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,7 +15,7 @@ public class ModEntityTypeTagsProvider extends EntityTypeTagsProvider {
 
     @Override
     protected void addTags() {
-        tag(ModularRoutersTags.EntityTypes.activatorInteractBlacklist);
+        tag(ModularRoutersTags.EntityTypes.activatorInteractBlacklist).add(EntityType.VILLAGER, EntityType.WANDERING_TRADER);
         tag(ModularRoutersTags.EntityTypes.activatorAttackBlacklist);
     }
 }
