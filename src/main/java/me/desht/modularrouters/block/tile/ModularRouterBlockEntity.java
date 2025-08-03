94b5847e71c17752d6f5b982f2ebeb8e9408d743
Des Herriott <des.herriott@gmail.com>
2025-08-03 12:05:39 +0100

    import cleanup

--- src/main/java/me/desht/modularrouters/block/tile/ModularRouterBlockEntity.java
+++ src/main/java/me/desht/modularrouters/block/tile/ModularRouterBlockEntity.java
@@ -68,7 +68,6 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.AABB;
-import net.neoforged.neoforge.capabilities.ItemCapability;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.energy.EnergyStorage;
 import net.neoforged.neoforge.energy.IEnergyStorage;
