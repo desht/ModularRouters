33a7a2f05ce498ce9cd505cee095a747dbfab5b2
Des Herriott <des.herriott@gmail.com>
2026-05-11 12:38:32 +0100

    fixed translation key for keybindings category

--- src/main/java/me/desht/modularrouters/client/KeyBindings.java
+++ src/main/java/me/desht/modularrouters/client/KeyBindings.java
@@ -9,12 +9,12 @@ import net.neoforged.neoforge.client.settings.KeyConflictContext;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
-    public static KeyMapping.Category CATEGORY = new KeyMapping.Category(ModularRouters.id("key"));
+    public static KeyMapping.Category CATEGORY = new KeyMapping.Category(ModularRouters.id("gui"));
 
     public static final KeyMapping keybindConfigure = new KeyMapping("key.modularrouters.configure", KeyConflictContext.GUI,
-            InputConstants.getKey(new KeyEvent(GLFW.GLFW_KEY_C, -1, 0)), CATEGORY);
+            InputConstants.Type.KEYSYM, InputConstants.KEY_C, CATEGORY);
     public static final KeyMapping keybindModuleInfo = new KeyMapping("key.modularrouters.moduleInfo", KeyConflictContext.GUI,
-            InputConstants.getKey(new KeyEvent(GLFW.GLFW_KEY_I, -1, 0)), CATEGORY);
+            InputConstants.Type.KEYSYM, InputConstants.KEY_C, CATEGORY);
 
     public static void registerKeyBindings(RegisterKeyMappingsEvent event) {
         event.registerCategory(CATEGORY);
